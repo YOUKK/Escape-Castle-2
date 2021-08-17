66a26fa6e90d2ac3500d16b5ae9af5e67f0fc236
Dong-kyu-Lee <stvboy00@gmail.com>
2021-08-17 13:01:05 +0900

    Make Monster Move Script

---
+++ Assets/Scripts/MonsterMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterMove : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] waypoints;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        //목표 지점까지의 방향 벡터
+        Vector2 waypointDir = (waypoints[0].position - transform.position).normalized;
+        
+    }
+}
