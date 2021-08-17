83599a095afbd2bd2f5f5532a08c88fac9f7e945
KimYouKyeong <kimyoukeng@naver.com>
2021-08-17 21:25:10 +0900

    add Item script

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Item들의 공통 특징
+public class Item : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+	private void OnTriggerStay2D(Collider2D collision)
+	{
+		if (Input.GetKeyDown(KeyCode.F))
+		{
+            Destroy(this.gameObject);
+		}
+	}
+}
