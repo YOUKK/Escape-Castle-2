d66348da7292b958db74f5f5a4e6046f181cb178
Dong-kyu-Lee <stvboy00@gmail.com>
2021-08-17 15:49:05 +0900

    Implement monster follow the Player

--- Assets/Scripts/MonsterMove.cs
+++ Assets/Scripts/MonsterMove.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class MonsterMove : MonoBehaviour
 {
+    int currentWaypoint;
+
     [SerializeField]
     private Transform[] waypoints;
+
+    [SerializeField]
+    private float monsterSpeed;
 
     void Start()
     {
@@ -19,8 +24,14 @@ public class MonsterMove : MonoBehaviour
 
     void Move()
     {
+        if (Vector2.Distance(waypoints[currentWaypoint].position, transform.position) < 0.05f)
+        {
+            //waypoint 설정
+            currentWaypoint = (currentWaypoint + 1) % waypoints.Length;
+        }
         //목표 지점까지의 방향 벡터
-        Vector2 waypointDir = (waypoints[0].position - transform.position).normalized;
-        
+        Vector2 waypointDir = (waypoints[currentWaypoint].position - transform.position).normalized;
+        //이동
+        transform.Translate(waypointDir * monsterSpeed * Time.deltaTime);
     }
 }
