da25355df62668504632ed524cc2eeb00e1a8753
KimYouKyeong <kimyoukeng@naver.com>
2021-08-19 01:34:31 +0900

    make dash bar UI and other item, make Long Board script and Dash script

---
+++ Assets/Scripts/Dash UI.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DashUI : MonoBehaviour
+{
+    // dash
+    [SerializeField]
+    private int dash; // dash 총량
+    private int currentDash; // 현재 dash량
+
+    // dash바 증가량
+    [SerializeField]
+    private int dashIncrease;
+
+    // dash바 재회복 딜레이
+    [SerializeField]
+    private int dashRechargeTime; // dash바가 회복되는데 걸리는 시간(dash를 사용하지 말아야하는 총 시간)
+    private int currentDashRechargeTime; // dash를 사용하지 않은 시간
+
+    // dash바 감소 여부
+    private bool dashUsed = false;
+
+    // 필요한 이미지
+    [SerializeField]
+    private Image dashBarImage;
+
+    void Start()
+    {
+        currentDash = dash;
+    }
+
+    void Update()
+    {
+        DashRecover();
+        DashRechargeTime();
+        GaugeUpdate();
+    }
+
+    private void DecreaseDash(int count)
+	{
+        dashUsed = true;
+        currentDashRechargeTime = 0;
+
+        if(currentDash - count > 0)
+		{
+            currentDash -= count;
+		}
+		else
+		{
+            currentDash = 0;
+		}
+	}
+
+    private void DashRecover()
+	{
+        if(!dashUsed && (currentDash < dash))
+		{
+            currentDash += dashIncrease;
+		}
+	}
+
+    private void DashRechargeTime()
+	{
+		if (dashUsed)
+		{
+            if(currentDashRechargeTime < dashRechargeTime)
+			{
+                currentDashRechargeTime++;
+			}
+			else
+			{
+                dashUsed = false;
+			}
+		}
+	}
+
+    private void GaugeUpdate()
+	{
+        dashBarImage.fillAmount = (float)currentDash / dash;
+	}
+}
--- Assets/Scripts/Item/Item.cs
+++ Assets/Scripts/Item/Item.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 // Item들의 공통 특징
 public class Item : MonoBehaviour
 {
+	// 아이템 획득 설명 텍스트
     [SerializeField]
     Text pickUpText;
 
@@ -19,15 +20,28 @@ public class Item : MonoBehaviour
         TryPickUp();
     }
 
+
+	// 플레이어와 아이템이 enter일 때
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
-		//if (collision.gameObject.tag.Equals("Player"))
-		//{
+		if (collision.gameObject.tag.Equals("Player"))
+		{
 			Debug.Log(collision.name + "enter");
-            pickUpText.gameObject.SetActive(true);
-		//}
+            //pickUpText.gameObject.SetActive(true);
+		}
 	}
 
+	// 플레이어와 아이템이 stay일 때
+	private void OnTriggerStay2D(Collider2D collision)
+	{
+		if (collision.gameObject.tag.Equals("Player"))
+		{
+			Debug.Log(collision.name + "stay");
+			pickUpText.gameObject.SetActive(true);
+		}
+	}
+
+	// 플레이어와 아이템이 exit일 때
 	private void OnTriggerExit2D(Collider2D collision)
 	{
 		if (collision.gameObject.tag.Equals("Player"))
@@ -37,6 +51,7 @@ public class Item : MonoBehaviour
 		}
 	}
 
+	// 아이템 줍기 시도
 	private void TryPickUp()
 	{
 		if (Input.GetKeyDown(KeyCode.F))
@@ -45,6 +60,7 @@ public class Item : MonoBehaviour
 		}
 	}
 
+	// 아이템 줍기
     private void PickUp()
 	{
         Destroy(this.gameObject);
