f31d3b1e1d27186562bce5ca4a2d99b14d15543d
Dong-kyu-Lee <stvboy00@gmail.com>
2021-08-21 20:20:42 +0900

    Please

--- Assets/Scripts/Item/ItemList.cs
+++ Assets/Scripts/Item/ItemList.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "New Item", menuName = "New Item/item")]
 public class ItemList : ScriptableObject
 {
 	public string itemName;
 	public ItemType itemtype;
 	public Sprite itemImage;
+	public GameObject itemPrefab;
+	
 
 	public enum ItemType
 	{
