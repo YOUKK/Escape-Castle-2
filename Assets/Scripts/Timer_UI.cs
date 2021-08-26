5cf71f71f94e37a017bc22a041a031b7a413141e
Dong-kyu-Lee <stvboy00@gmail.com>
2021-08-26 23:01:47 +0900

    Make Timer Script

--- Assets/Scripts/Timer_UI.cs
+++ Assets/Scripts/Timer_UI.cs
@@ -8,13 +8,33 @@ public class Timer_UI : MonoBehaviour
     [SerializeField] Text minute;
     [SerializeField] Text second;
 
+    [SerializeField] int min;
+    [SerializeField] float sec;
+
     void Start()
     {
-        
+        minute.text = min.ToString();
+        second.text = sec.ToString();
     }
 
     void Update()
     {
-        
+        if (min != 0)
+        {
+            --min;
+            if (sec == 0)
+            {
+                sec += 60;
+            }
+            sec -= Time.deltaTime;
+        }
+        else if(min == 0 && sec == 0)
+        {
+            min = 0;
+            sec = 0;
+        }
+
+        minute.text = min.ToString();
+        second.text = Mathf.Ceil(sec).ToString();
     }
 }
