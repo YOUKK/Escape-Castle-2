f76b6084a5616aebcdd9d697b1b62816e5f17e45
Dong-kyu-Lee <stvboy00@gmail.com>
2021-08-26 21:40:39 +0900

    Modify UI position and Make Timer

---
+++ Assets/Scripts/Timer_UI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer_UI : MonoBehaviour
+{
+    [SerializeField] Text minute;
+    [SerializeField] Text second;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
