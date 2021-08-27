65f3a33b0d5382452761fbb6dc30d200fe25736a
KimYouKyeong <kimyoukeng@naver.com>
2021-08-27 15:05:07 +0900

    add sound

---
+++ Assets/Scripts/Sound/SkeletonSound.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonSound : MonoBehaviour
+{
+    public AudioClip WalkSound;
+    private AudioSource theAudioSource;
+
+    private float walkTimer;
+    private float walkWaitingTime;
+
+    void Start()
+    {
+        theAudioSource = GetComponent<AudioSource>();
+
+        walkTimer = 0f;
+        walkWaitingTime = 0.3f;
+    }
+
+    void Update()
+    {
+        walkTimer += Time.deltaTime;
+        if (walkTimer > walkWaitingTime)
+        {
+            WalkSoundPlayer();
+            walkTimer = 0;
+        }
+    }
+
+    private void WalkSoundPlayer()
+	{
+        theAudioSource.clip = WalkSound;
+        theAudioSource.Play();
+	}
+}
