425f6ed1b4377372c48eae41b6705bd4e3f1a35b
KimYouKyeong <kimyoukeng@naver.com>
2021-08-30 19:30:12 +0900

    complete dead ending

--- Assets/Scripts/Ending_Dead.cs
+++ Assets/Scripts/Ending_Dead.cs
@@ -1,21 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Ending_Dead : MonoBehaviour
 {
+	//public static Ending_Dead instance;
+
     // 다른 스크립트 사용
 	[SerializeField]
     private PlayerController thePlayerController;
 
     public GameObject Ending;
-    //public AudioManager theAudioManager;
+	//public AudioManager theAudioManager;
 
 	//public string 브금;
+
+	//private void Awake()
+	//{
+	//	if(instance == null)
+	//	{
+	//		DontDestroyOnLoad(this.gameObject);
+	//		instance = this;
+	//	}
+	//	else
+	//	{
+	//		Destroy(this.gameObject);
+	//	}
+	//}
 
 	public void Exit()
 	{
         Application.Quit();
+	}
+
+	public void Reload()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 	}
 
 	private void Start()
@@ -27,7 +48,7 @@ public class Ending_Dead : MonoBehaviour
 	{
 		if (this.gameObject.activeInHierarchy)
 		{
-
+			// 브금 재생
 		}
 	}
 
