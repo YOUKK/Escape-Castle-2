1703734c4301e59ccdc0748324971dc5b15425d8
Dong-kyu-Lee <stvboy00@gmail.com>
2021-08-30 20:08:04 +0900

    Make First Scene Light and UI Image

---
+++ Assets/Scripts/UI/Light_UI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class Light_UI : MonoBehaviour
+{
+    Light2D light2D;
+    [SerializeField] float minDelayTime;
+    [SerializeField] float maxdDelayTime;
+
+    [SerializeField] float minIntensity;
+    [SerializeField] float maxIntensity;
+
+    float count = 0;
+
+    void Start()
+    {
+        light2D = GetComponent<Light2D>();
+    }
+
+    void Update()
+    {
+        count += Time.deltaTime;
+        if(count >= Random.Range(minDelayTime, maxdDelayTime))
+        {
+            light2D.intensity = Random.Range(minIntensity, maxIntensity);
+            count = 0;
+        }
+    }
+}
